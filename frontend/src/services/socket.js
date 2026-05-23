097f5259016648207ce7f9d0d13845cf0063f69e
yasir arfat <yasir.arfat.cse@gmail.com>
2026-05-23 14:36:19 +0530

    frontend connected to render backend

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:3000"; // backend URL
+const SOCKET_URL = import.meta.env.VITE_API_URL; // backend URL
 
 const socket = io(SOCKET_URL, {
   autoConnect: false,
