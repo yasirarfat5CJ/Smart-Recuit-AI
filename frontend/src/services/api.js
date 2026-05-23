b86f903b027eb3261802e1a9a39f0b3ea8cc3420
yasir arfat <yasir.arfat.cse@gmail.com>
2026-05-23 15:34:22 +0530

    fix upload issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 API.interceptors.request.use((config) => {
